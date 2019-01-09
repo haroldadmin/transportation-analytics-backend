ccb46275574e78d6c9563c8d308cfd60c181663e
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-01-09 10:34:14 +0530

    feat: Add UserMoel for database

---
+++ database/models/user.py
@@ -0,0 +1,19 @@
+from database import db
+
+
+class UserModel(db.Model):
+    __tablename__ = "users"
+
+    def __init__(self, name, bio=""):
+        self.name = name
+        self.bio = bio
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    requests = db.relationship("RequestModel", backref="requests", lazy=True)
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
