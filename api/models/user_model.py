01437653f638aa41758e6564c0356bfeed81218f
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-01-09 10:47:55 +0530

    feat: Add Users resource

--- api/models/user_model.py
+++ api/models/user_model.py
@@ -1,5 +1,10 @@
 from flask_restplus import fields, Model
 
+
+def add_models_to_namespace(namespace):
+    namespace.models[user_model.name] = user_model
+
+
 user_model = Model("Represents a user on our system", {
     "name": fields.String(description="Name of the user"),
     "email": fields.String(description="Email of the user")
---
+++ api/resources/users.py
@@ -0,0 +1,50 @@
+from flask import request
+from flask_restplus import Namespace, Resource, marshal
+
+from api.models import user_model
+from api.models.user_model import add_models_to_namespace
+from database.models.user import UserModel
+
+ns = Namespace("users", "Operations related to users")
+add_models_to_namespace(ns)
+
+
+@ns.route("/")
+class UserCollection(Resource):
+
+    @classmethod
+    @ns.marshal_with(user_model)
+    def get(cls):
+        return UserModel.query.all()
+
+
+@ns.route("/<int:user_id>")
+@ns.param("user_id", "The ID of the requested user")
+class UserProfile(Resource):
+
+    @classmethod
+    def get(cls, user_id):
+        user = UserModel.query.get(user_id)
+        if not user:
+            return {
+                       "message": "User not found"
+                   }, 404
+
+        return marshal(user, user_model), 200
+
+
+@ns.route("users")
+class UserRegister(Resource):
+
+    @classmethod
+    @ns.marshal_with(user_model)
+    def post(cls):
+        data = request.json
+
+        name = data["name"]
+        bio = data["bio"]
+
+        user = UserModel(name, bio)
+        user.save_to_db()
+
+        return user, 200
