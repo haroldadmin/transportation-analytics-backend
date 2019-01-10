3d06517469acd2f0ecc9d7ca5059017259281364
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-01-10 16:34:58 +0530

    fix: Fix local network invisibility problem

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 
 
 def configure_app(flask_app):
-    flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
     flask_app.config['JWT_SECRET_KEY'] = settings.JWT_SECRET_KEY
@@ -33,4 +32,4 @@ def create_tables():
 
 if __name__ == "__main__":
     initialize_app(app)
-    app.run(host="0.0.0.0", debug=settings.FLASK_DEBUG)
+    app.run(host="0.0.0.0", port=settings.PORT, debug=settings.FLASK_DEBUG)
