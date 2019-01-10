f480095afb789c07213d528217d7a090c49897e6
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-01-10 12:01:29 +0530

    feat: Add password hash support to UserModel

--- database/models/user.py
+++ database/models/user.py
@@ -1,3 +1,5 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from database import db
 
 
@@ -10,10 +12,16 @@ class UserModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, nullable=False)
     requests = db.relationship("RouteRequestModel", backref="user", lazy=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password_hash = db.Column(db.String, nullable=False)
+
+    def set_password(self, password_plain_text):
+        self.password_hash = generate_password_hash(password_plain_text)
+
+    def check_password(self, password_plain_text):
+        return check_password_hash(self.password_hash, password_plain_text)
 
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
-
