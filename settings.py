0c9620a6a36534b8ba013bf57aaf89b001aea211
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-01-10 16:34:20 +0530

    fix: Fix local network invisibility problem

--- settings.py
+++ settings.py
@@ -6,8 +6,9 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Flask settings
-FLASK_SERVER_NAME = 'localhost:3000'
+# FLASK_SERVER_NAME = 'localhost:3000'
 FLASK_DEBUG = True  # Do not use debug mode in production
+PORT = 3000
 
 # JWT
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
